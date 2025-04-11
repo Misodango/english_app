05bf750dafd8117a9a8d7dcf5ad1a8dd4f7ece36
Misodango <kazu.kazu.tub@gmail.com>
2025-04-11 13:55:58 +0900

    firebase deploy config

--- english-app/src/firebase/init.js
+++ english-app/src/firebase/init.js
@@ -1,12 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
@@ -16,8 +12,8 @@ const firebaseConfig = {
   appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 export { db }
-// const analytics = getAnalytics(app);
--- english-app/src/router/index.js
+++ english-app/src/router/index.js
@@ -36,7 +36,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
+  // history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
